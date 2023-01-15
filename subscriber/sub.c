9d3545f4efcbb30ac07409905fa0a8e07a3538ad
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2023-01-15 11:42:08 +0000

    Clean publisher and subscriber executables and delete professor's filesystem

--- subscriber/sub.c
+++ subscriber/sub.c
@@ -10,71 +10,58 @@
 #include <stdlib.h>
 #include <signal.h>
 
-int fd_fifo;
+int fd;
 char* pipe_name;
 int counter = 0;
 
 void getCTRLC(int s) {
     (void) s;
     char buffer[100];
+    // Why is this here ???
     int value = snprintf(buffer, sizeof(buffer) - 1, "%d", counter);
-    if (write(STDOUT_FILENO,"ESTOU NO SIGNAL\n",strlen("ESTOU NO SGINAL\n")) < 0) {
-        exit(1);
-    }
     if (write(STDOUT_FILENO, buffer, (size_t)value) < 0) {
         exit(1);
     }
-    close(fd_fifo);
+    close(fd);
+    // Why is this here ???
     signal(SIGINT, getCTRLC);
     unlink(pipe_name);
     exit(0);
 }
 
+// Não se devia ligar o signal SIGPIPE a isto ???
 void ignore_sigpipe(int s) {
     (void) s;
-
 }
 
 int main(int argc, char **argv) {
     (void)argc;
-    (void)argv;
     pipe_name = argv[2];
     request newrequest;
     newrequest.code = 2;
-    strcpy(newrequest.pipe_name, argv[2]);
+    strcpy(newrequest.pipe_name, pipe_name);
     char box_name_slash[MAX_BOX_NAME];
     memset(box_name_slash, 0, MAX_BOX_NAME);
     strcpy(box_name_slash, "/");
     strcat(box_name_slash, argv[3]);
     strcpy(newrequest.box_name, box_name_slash);
-    char buffer[MAX_LINE];
-    writer_stc(&newrequest, newrequest.code, buffer);
-    printf("Mensagem a mandar no pipe %s tamanho %ld \n", buffer, strlen(buffer));
-    int fd = open(argv[1], O_WRONLY);
-    ssize_t value = write(fd, buffer, sizeof(buffer));
-    printf("Tamanho que foi escrito %ld \n", value);
-    value++;
+    char server_request[MAX_LINE];
+    writer_stc(&newrequest, newrequest.code, server_request);
+    if ((fd = open(argv[1], O_WRONLY)) < 0) exit(1);
+    if (write(fd, server_request, sizeof(server_request)) < 0) exit(1); 
     close(fd);
-    if (mkfifo(argv[2], 0777) < 0) {
-        exit(1);
-    }
+    if (mkfifo(argv[2], 0777) < 0) exit(1);
     signal(SIGINT, getCTRLC);
-    fd_fifo = open(argv[2], O_RDONLY);
-    if (fd_fifo < 0) {exit(1);}
+    if ((fd = open(argv[2], O_RDONLY)) < 0) exit(1);
     while (1) {
         char message[MAX_LINE];
-        value = read(fd_fifo, message, sizeof(message));
-        if (value == 0) {
-            break;
-        }
+        if (read(fd, message, sizeof(message)) == 0) break;
         counter++;
-        value++;
-        //strtok(message, "|");
         messages_pipe* newmesage = reader_stc(message);
         fprintf(stdout, "%s\n", newmesage->message);
         free(newmesage);
     }
-    close(fd_fifo);
-    unlink(argv[2]);
+    close(fd);
+    unlink(pipe_name);
     return -1;
 }
